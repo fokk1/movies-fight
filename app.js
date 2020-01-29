fea0e6b66949bf0ca5f9ac36d77f42928c0e033a
fokk1 <metla40@gmail.com>
2020-01-29 22:13:16 +0300

    The second commit.
    The movies description has been added.

--- app.js
+++ app.js
@@ -38,15 +38,21 @@ const onInput = async (event) => {
 
 	results.innerHTML = '';
 	dropdown.classList.add('is-active');
-	for (movie of movies) {
-		const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+	for (let movie of movies) {
 		const option = document.createElement('a');
+		const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
 
 		option.classList.add('dropdown-item');
 		option.innerHTML = `
-			<img class="poster" src="${imgSrc}">
-			<div>${movie.Title}, ${movie.Year}</div>
-		`;
+		<img class="poster" src="${imgSrc}" />
+		<div>${movie.Title}, ${movie.Year}</div>
+	  `;
+		option.addEventListener('click', () => {
+			dropdown.classList.remove('is-active');
+			input.value = movie.Title;
+			onMovieSelect(movie);
+		});
+
 		results.append(option);
 	}
 };
@@ -58,3 +64,52 @@ document.addEventListener('click', (event) => {
 		dropdown.classList.remove('is-active');
 	}
 });
+
+const onMovieSelect = async (movie) => {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params: {
+			apikey: 'd9d84edd',
+			i: movie.imdbID
+		}
+	});
+	document.querySelector('#test').innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = (movieDetail) => {
+	return `
+	<div class="media">
+		<figure class="media-left">
+			<p class="image">
+				<img src="${movieDetail.Poster}" alt="">
+			</p>
+		</figure>
+		<div class="media-content">
+			<div class="content">
+				<h1>${movieDetail.Title}</h1>
+				<h4>${movieDetail.Genre}</h4>
+				<p>${movieDetail.Plot}</p>
+			</div>
+		</div>
+	</div>
+	<div class="notification is-dark">
+		<p class="titile">${movieDetail.Awards}</p>
+		<p class="subtitile">Awards</p>
+	</div>
+	<div class="notification is-dark">
+		<p class="titile">${movieDetail.BoxOffice}</p>
+		<p class="subtitile">Box Office</p>
+	</div>
+	<div class="notification is-dark">
+		<p class="titile">${movieDetail.Metascore}</p>
+		<p class="subtitile">Metascore</p>
+	</div>
+	<div class="notification is-dark">
+		<p class="titile">${movieDetail.imdbRating}</p>
+		<p class="subtitile">IMDB Rating</p>
+	</div>
+	<div class="notification is-dark">
+		<p class="titile">${movieDetail.imdbVotes}</p>
+		<p class="subtitile">IMDB Votes</p>
+	</div>
+	`;
+};
